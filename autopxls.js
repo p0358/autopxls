e10277fe5ed1c5da796b23ed8ee4a7cd14a166ff
p0358 <p0358@users.noreply.github.com>
2017-04-14 20:40:37 +0200

    Now this should finally work :)

--- autopxls.js
+++ autopxls.js
@@ -22,9 +22,9 @@ window.App.socket.send = function (data) { // thanks for giving me the idea :)
 }
 // Oh, would forget about the other 2!
 window.App.socketclose = window.App.socket.close;
-window.App.socket.close = function() { if (arguments.callee.caller.name.toLowerCase().indexOf('ban') == -1 || arguments.callee.caller.name == 'b') return window.App.socketclose(); }
+window.App.socket.close = function() { if (!(arguments.callee.caller.name.toLowerCase().indexOf('ban') != -1 || arguments.callee.caller.name == 'b')) return window.App.socketclose(); }
 window.locationreload = window.location.reload;
-window.location.reload = function() { if (arguments.callee.caller.name.toLowerCase().indexOf('ban') == -1 || arguments.callee.caller.name == 'b') return window.locationreload(); }
+window.location.reload = function() { if (!(arguments.callee.caller.name.toLowerCase().indexOf('ban') != -1 || arguments.callee.caller.name == 'b')) return window.locationreload(); }
 
 document.autoPxlsScriptRevision = 10;
 if (!document.autoPxlsRandomNumber) document.autoPxlsRandomNumber = Math.round(Math.random() * 10000000);
